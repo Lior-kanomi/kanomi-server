606f3cb167fbb734dcb7b273a9be3d90b6d10395
Lior-kanomi <lior.kanomi@gmail.com>
2022-12-27 10:15:42 +0200

    Replace the relative path '.api/users' with dynamic one

--- server.js
+++ server.js
@@ -3,19 +3,15 @@ const express = require('express');
 const nodemon = require('nodemon');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/userRoute');
+const path = require("path");
 const apiRoutes = require('./routes/apiRoutes');
 const cors = require('cors');
-const openssl = require('openssl-nodejs');
+
 
 const app = express();
 
-// Disable SSL certificate validation
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
 // Load the environment variables from the .env file
 dotenv.config();
-
-const db = require('./data/db');
 
 // Create an express app
 app.use(cors());
@@ -26,7 +22,7 @@ const port = process.env.PORT || 5000;
 
 // Mount the routes to the express app
 app.use('/', (req,res) => res.status(200).json({message:"Success",data:[]}));
-app.use('/api/users', userRouter);
+app.use(path.join(__dirname,'/api/users'), userRouter);
 
 
 // Start the server
