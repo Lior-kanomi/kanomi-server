c2336378101adc92e937fd2be6d7bb083bfa56f7
Lior-kanomi <lior.kanomi@gmail.com>
2022-12-28 19:31:10 +0200

    ..

--- controllers/ButtonController.js
+++ controllers/ButtonController.js
@@ -59,9 +59,7 @@ exports.getIcon = async (req, res) => {
     }
     if (button) {
       console.log(button);
-      return res
-        .status(200)
-        .json({ message: "Success", icon: button.data.icon.data });
+      return res.status(200).json({ message: "Success", icon: button.icon });
     }
     return res
       .status(400)
