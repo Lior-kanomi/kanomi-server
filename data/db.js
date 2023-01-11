8cf16d39a9d198b85731976ab416e6e76c407c40
Lior-kanomi <lior.kanomi@gmail.com>
2023-01-11 17:26:53 +0200

    Remove TIMES env variable from the .env file, and add a new error handler to the db.js file

--- data/db.js
+++ data/db.js
@@ -1,18 +1,34 @@
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', true);
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-mongoose.connection.on('connected', () => {
-  console.log('MongoDB connection established!');
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connection established!");
 });
 
-mongoose.connection.on('error', (error) => {
+mongoose.connection.on("error", (error) => {
   console.error(`MongoDB connection error: ${error}`);
   process.exit(-1);
 });
 
+//New Unhandled exceptions method
+mongoose.connection.on("disconnected", (error) => {
+  if (error) {
+    console.error(`MongoDB connection disconnected: ${error}`);
+    process.exit(-1);
+  } else {
+    console.log("MongoDB connection disconnected through app termination");
+    process.exit(0);
+  }
+});
 
+process.on("SIGINT", () => {
+  mongoose.connection.close(() => {
+    console.log("Mongoose connection disconnected through app termination");
+    process.exit(0);
+  });
+});
