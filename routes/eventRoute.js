cbbaa6b4f0a1db777f5304423cbd08d915b8e204
Lior-kanomi <lior.kanomi@gmail.com>
2023-02-20 18:57:21 +0200

    Add an engine to the EventRoute file

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -10,5 +10,8 @@ router.use((req, res, next) => {
   res.status(404).render("error");
 });
 
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
 // Exports the router to be used in the main application
 module.exports = router;
