a36d5c781a6d1879026622044ae887311bd4aead
Lior-kanomi <lior.kanomi@gmail.com>
2023-02-20 18:41:33 +0200

    Add an event controller

---
+++ controllers/EventController.js
@@ -0,0 +1,35 @@
+const Event = require("../models/Button");
+
+// Create a new user and save it to the database
+exports.createButton = async (req, res) => {
+  Event.findOne({ eventName: req.body.buttonName }, (error, event) => {
+    if (error) {
+      return res.status(500).json({ message: error.message });
+    }
+    if (!event) {
+      const { properties, date, type } = req.body;
+      const newEvent = new Event({ properties, date, type });
+      Button.create(newEvent)
+        .then((createdEvent) => {
+          // If the Event was successfully created, send a 200 OK response with the created Button document
+          res.status(200).json({
+            message: "Event created successfully",
+            data: createdEvent,
+          });
+        })
+        .catch((error) => {
+          // If there is an error, send a 500 error response
+          res.status(500).send(error);
+        });
+    } else {
+      event.save((error) => {
+        if (error) {
+          return res.status(500).json({ message: error.message });
+        }
+        res
+          .status(200)
+          .json({ message: "Event created successfully", data: event });
+      });
+    }
+  });
+};
