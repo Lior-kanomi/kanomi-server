5c8baf6e3c3ea9581b3b28f4287bfd47bc11eb9c
Lior-kanomi <lior.kanomi@gmail.com>
2023-02-21 10:44:54 +0200

    Remove the if block so it always create a doc

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -8,35 +8,24 @@ exports.createEvent = async (req, res) => {
     if (error) {
       return res.status(500).json({ message: error.message });
     }
-    if (!event) {
-      console.log("Inside the event's creation block");
-      const { properties, eventName } = req.body;
-      console.log(`destruction of the req.body ${properties} ${eventName}`);
-      const newEvent = new Event({ properties, eventName });
-      console.log(`New event has been created ${newEvent}`);
+    console.log("Inside the event's creation block");
+    const { properties, eventName } = req.body;
+    console.log(`destruction of the req.body ${properties} ${eventName}`);
+    const newEvent = new Event({ properties, eventName });
+    console.log(`New event has been created ${newEvent}`);
 
-      Event.create(newEvent)
-        .then((createdEvent) => {
-          console.log(`Event has been stored in DB ${newEvent}`);
-          // If the Event was successfully created, send a 200 OK response with the created Button document
-          res.status(200).json({
-            message: "Event created successfully",
-            data: createdEvent,
-          });
-        })
-        .catch((error) => {
-          // If there is an error, send a 500 error response
-          res.status(500).send(error);
+    Event.create(newEvent)
+      .then((createdEvent) => {
+        console.log(`Event has been stored in DB ${newEvent}`);
+        // If the Event was successfully created, send a 200 OK response with the created Button document
+        res.status(200).json({
+          message: "Event created successfully",
+          data: createdEvent,
         });
-    } else {
-      event.save((error) => {
-        if (error) {
-          return res.status(500).json({ message: error.message });
-        }
-        res
-          .status(200)
-          .json({ message: "Event created successfully", data: event });
+      })
+      .catch((error) => {
+        // If there is an error, send a 500 error response
+        res.status(500).send(error);
       });
-    }
   });
 };
