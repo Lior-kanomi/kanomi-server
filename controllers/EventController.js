963a7075e3acc6f95530118abffd466b2c2ad856
Lior-kanomi <lior.kanomi@gmail.com>
2023-02-21 10:23:45 +0200

    Add prints to the Event controller

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -2,15 +2,19 @@ const Event = require("../models/Button");
 
 // Create a new user and save it to the database
 exports.createEvent = async (req, res) => {
+  console.log("fired!");
   Event.findOne({ eventName: req.body.eventName }, (error, event) => {
     if (error) {
       return res.status(500).json({ message: error.message });
     }
     if (!event) {
-      const { properties, type } = req.body;
-      const newEvent = new Event({ properties, type });
+      const { properties, eventName } = req.body;
+      const newEvent = new Event({ properties, eventName });
+      console.log(`New event has been created ${newEvent}`);
+
       Event.create(newEvent)
         .then((createdEvent) => {
+          console.log(`Event has been stored in DB ${newEvent}`);
           // If the Event was successfully created, send a 200 OK response with the created Button document
           res.status(200).json({
             message: "Event created successfully",
