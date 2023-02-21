a1f4c37f53bf8fc7a80faac81e5ca6437715cdfa
Lior-kanomi <lior.kanomi@gmail.com>
2023-02-21 13:43:38 +0200

    Uncomment the "user_id" field

--- models/Event.js
+++ models/Event.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const eventSchema = new mongoose.Schema({
-  // user_id: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: "User",
-  // },
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
   eventName: {
     type: String,
     enum: [
