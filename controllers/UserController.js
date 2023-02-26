b8d8b8a880677b90d4b3e71d0883b0b6c66ba2f0
Lior-kanomi <lior.kanomi@gmail.com>
2023-02-26 14:28:22 +0200

    Add a user controller method.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -25,3 +25,29 @@ exports.createUser = async (req, res) => {
     res.status(500).send(error);
   }
 };
+
+exports.addUserError = async (req, res) => {
+  try {
+    console.log(req.body);
+    const { _id } = req.body;
+    if (!_id || !operating_system_version) {
+      return res.status(400).json({ message: "Missing fields" });
+    }
+    const newUser = new User({
+      _id,
+      operating_system_version,
+    });
+
+    console.log(`Before the create of the User`, _id, operating_system_version);
+    const createdUser = await User.create(newUser);
+
+    // If the User was successfully created, send a 200 OK response with the created User document
+    res.status(200).json({
+      message: "User created successfully",
+      data: createdUser,
+    });
+  } catch (error) {
+    // If there is an error, send a 500 error response
+    res.status(500).send(error);
+  }
+};
