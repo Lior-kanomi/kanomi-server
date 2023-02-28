4a995e30a749254c14aed082e3bec97df2fac25d
Lior-kanomi <lior.kanomi@gmail.com>
2023-02-28 13:20:54 +0200

    Add a new route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,6 +5,9 @@ const userController = require("../controllers/UserController");
 // Route for adding a new Event
 router.post("/addUser", userController.createUser);
 
+// Route for deleting all the events
+router.delete("/deleteUsers", userController.deleteUsers);
+
 // Define a catch-all route to handle invalid routes
 router.use((req, res, next) => {
   res.status(404).render("error");
