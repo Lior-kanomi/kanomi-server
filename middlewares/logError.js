8e2f22d93372b25d796da42b8e953f0d82b59ce3
Lior-kanomi <lior.kanomi@gmail.com>
2023-02-28 13:38:14 +0200

    Prints statements

--- middlewares/logError.js
+++ middlewares/logError.js
@@ -2,6 +2,8 @@ const User = require("../models/User");
 
 const logError = async (req, res, next) => {
   try {
+    console.log(req.body);
+    console.log(req.body.properties);
     console.log(req.body.properties.Error);
     // if (req.body.properties.Error) {
     //   const userId = req.body.user._id;
