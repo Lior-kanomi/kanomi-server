eb741c696fdb6b55d051fe976b1f81f67ba8a816
Lior-kanomi <lior.kanomi@gmail.com>
2023-02-28 13:32:15 +0200

    Comment the middleware if block

--- middlewares/logError.js
+++ middlewares/logError.js
@@ -3,17 +3,17 @@ const User = require("../models/User");
 const logError = async (req, res, next) => {
   try {
     console.log(req.body);
-    if (req.body.properties.Error) {
-      const userId = req.body.user._id;
-      const errorMessage = req.error.message;
+    // if (req.body.properties.Error) {
+    //   const userId = req.body.user._id;
+    //   const errorMessage = req.error.message;
 
-      await User.updateOne(
-        { _id: userId },
-        {
-          $push: { error_logs: { message: errorMessage } },
-        }
-      );
-    }
+    //   await User.updateOne(
+    //     { _id: userId },
+    //     {
+    //       $push: { error_logs: { message: errorMessage } },
+    //     }
+    //   );
+    // }
 
     next();
   } catch (err) {
