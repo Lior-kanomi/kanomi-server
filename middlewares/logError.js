2e6d4e6473861514f24ad92e06efca77ed289bba
Lior-kanomi <lior.kanomi@gmail.com>
2023-02-28 13:43:50 +0200

    Uncomment the logErorr middleware

--- middlewares/logError.js
+++ middlewares/logError.js
@@ -4,17 +4,17 @@ const logError = async (req, res, next) => {
   try {
     console.log(req.body);
     console.log(req.body.properties);
-    // if (req.body.properties.Error) {
-    //   const userId = req.body.user._id;
-    //   const errorMessage = req.error.message;
+    if (req.body.properties.Error) {
+      const userId = req.body.user._id;
+      const errorMessage = req.error.message;
 
-    //   await User.updateOne(
-    //     { _id: userId },
-    //     {
-    //       $push: { error_logs: { message: errorMessage } },
-    //     }
-    //   );
-    // }
+      await User.updateOne(
+        { _id: userId },
+        {
+          $push: { error_logs: { message: errorMessage } },
+        }
+      );
+    }
 
     next();
   } catch (err) {
