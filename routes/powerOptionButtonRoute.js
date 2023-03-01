3a4ec4b87ae450292ff164a8e28f56bbc0d0ed50
Lior-kanomi <lior.kanomi@gmail.com>
2023-03-01 17:43:38 +0200

    Rename the controller method

--- routes/powerOptionButtonRoute.js
+++ routes/powerOptionButtonRoute.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const powerButtonController = require("../controllers/SettingButtonController");
+const powerButtonController = require("../controllers/PowerButtonController");
 
 // Route for adding a new button
 router.post(
   "/addPowerOptionButton",
-  powerButtonController.createSettingOptionButton
+  powerButtonController.createPowerOptionButton
 );
 // Define a catch-all route to handle invalid routes
 router.use((req, res, next) => {
