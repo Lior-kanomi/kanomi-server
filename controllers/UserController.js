5af4551969fd36e0a8eb858f080a94caa46973d0
Lior-kanomi <lior.kanomi@gmail.com>
2023-03-05 09:16:30 +0200

    Remove the _id field from the create user controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,16 +3,15 @@ const User = require("../models/User");
 exports.createUser = async (req, res) => {
   try {
     console.log(req.body);
-    const { _id, operating_system_version } = req.body;
-    if (!_id || !operating_system_version) {
+    const { operating_system_version } = req.body;
+    if (!operating_system_version) {
       return res.status(400).json({ message: "Missing fields" });
     }
     const newUser = new User({
-      _id,
       operating_system_version,
     });
 
-    console.log(`Before the create of the User`, _id, operating_system_version);
+    console.log(`Before the create of the User`, operating_system_version);
     const createdUser = await User.create(newUser);
 
     // If the User was successfully created, send a 200 OK response with the created User document
