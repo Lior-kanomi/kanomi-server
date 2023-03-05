0cdef6ab98b12325b8137b397490500ac410f70e
Lior-kanomi <lior.kanomi@gmail.com>
2023-03-05 10:01:20 +0200

    Change create method to save

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -18,7 +18,7 @@ exports.createUser = async (req, res) => {
       browser,
       operating_system_version
     );
-    const createdUser = await User.create(newUser);
+    const createdUser = await User.save(newUser);
     console.log(createdUser);
     // If the User was successfully created, send a 200 OK response with the created User document
     res.status(200).json({
