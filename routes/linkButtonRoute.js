5a7364762d735c587892b221c6850fb74c886138
Lior-kanomi <lior.kanomi@gmail.com>
2023-03-27 11:36:15 +0300

    Rename the linkButton route

--- routes/linkButtonRoute.js
+++ routes/linkButtonRoute.js
@@ -9,13 +9,10 @@ router.post("/addButton", buttonController.createButton);
 router.get("/getLink/:buttonName", buttonController.getLink);
 
 // Route for updating button's details
-router.post("/updateButtons", buttonController.updateButtons);
-
-// Route for getting the icon of a button based on its name
-router.get("/getImage/:buttonName", buttonController.getIcon);
+router.put("/updateButtons", buttonController.updateButtons);
 
 // Route for retrieving the link of a button based on its name
-router.get("/getLinks", buttonController.getLinks);
+router.get("/getLinksButtons", buttonController.getLinks);
 
 // Exports the router to be used in the main application
 module.exports = router;
