bd3efaf68117f6b2fe8cd4c5fdc616c073c21f5f
Lior-kanomi <lior.kanomi@gmail.com>
2023-03-27 11:27:14 +0300

    Comment the render method

--- server.js
+++ server.js
@@ -47,9 +47,9 @@ app.use("/api/event", eventRoute);
 app.use("/api/user", userRoute);
 
 // Define a catch-all route to handle invalid routes
-app.use((req, res, next) => {
-  res.status(404).render("error");
-});
+// app.use((req, res, next) => {
+//   res.status(404).render("error");
+// });
 
 // Start the server
 app.listen(port, () => {
