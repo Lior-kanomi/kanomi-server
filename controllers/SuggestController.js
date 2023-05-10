84327c77b95b42a0d523efdb84e2a142b06bd3c0
Lior-kanomi <lior.kanomi@gmail.com>
2023-05-10 19:11:24 +0300

    add another library

--- controllers/SuggestController.js
+++ controllers/SuggestController.js
@@ -1,15 +1,14 @@
 // Create a new user and save it to the database
-const axios = require("axios");
+const { fetch } = require("undici");
 const circularJSON = require("json-circular-stringify");
 exports.getUserSuggestions = async (req, res) => {
   try {
     const langCode = "iw";
     const countryCode = "us";
     const requestUrl = `https://www.google.com/complete/search?q=${req.body.query}&hl=${req.body.langCode}&client=chrome&gl=${req.body.countryCode}`;
-    const googleRes = await axios.get(requestUrl);
+    const googleRes = await fetch(requestUrl);
     const data = googleRes.json();
-    const jsonString = circularJSON.stringify(data);
-    res.json(jsonString);
+    res.json(data);
     res.status(200).json(googleRes);
   } catch (error) {
     res.status(500).send(error.message);
