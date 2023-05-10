518cb8d80f5c8fd6ab6072712a03133792b3e60e
Lior-kanomi <lior.kanomi@gmail.com>
2023-05-10 19:17:05 +0300

    return axios

--- controllers/SuggestController.js
+++ controllers/SuggestController.js
@@ -1,15 +1,12 @@
 // Create a new user and save it to the database
-const { fetch } = require("undici");
-const circularJSON = require("json-circular-stringify");
+const axios = require("axios");
 exports.getUserSuggestions = async (req, res) => {
   try {
     const langCode = "iw";
     const countryCode = "us";
     const requestUrl = `https://www.google.com/complete/search?q=${req.body.query}&hl=${req.body.langCode}&client=chrome&gl=${req.body.countryCode}`;
-    const googleRes = await fetch(requestUrl);
+    const googleRes = await axios.get(requestUrl);
     console.log(googleRes);
-    const data = googleRes.json();
-    console.log(data);
     res.status(200).json(googleRes);
   } catch (error) {
     res.status(500).send(error.message);
