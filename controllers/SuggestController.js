8e7fded87c6f286b30b5669f6204fe8de3a11fd6
Lior-kanomi <lior.kanomi@gmail.com>
2023-05-10 18:56:50 +0300

    add axios package

--- controllers/SuggestController.js
+++ controllers/SuggestController.js
@@ -4,8 +4,8 @@ exports.getUserSuggestions = async (req, res) => {
     const langCode = "iw";
     const countryCode = "us";
     const requestUrl = `https://www.google.com/complete/search?q=${req.body.query}&hl=${req.body.langCode}&client=chrome&gl=${req.body.countryCode}`;
-    const googleRes = await fetch(requestUrl);
-    res.status(200).json(googleRes);
+    const googleRes = await axios.get(requestUrl);
+    res.status(200).send(googleRes);
   } catch (error) {
     res.status(500).send(error.message);
   }
