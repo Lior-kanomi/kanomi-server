dc24750da4390192804621ecd6f12f87338a3259
Lior-kanomi <lior.kanomi@gmail.com>
2023-05-11 12:54:08 +0300

    Add a query

--- routes/autoSuggestRoute.js
+++ routes/autoSuggestRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const SuggestController = require("../controllers/SuggestController");
 
 // Route for adding a new button
-router.post("/getUserSuggestions", SuggestController.getUserSuggestions);
+router.get("/getUserSuggestions/:query", SuggestController.getUserSuggestions);
 
 // Exports the router to be used in the main application
 module.exports = router;
