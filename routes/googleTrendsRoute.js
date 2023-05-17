64d4db9b7b4fbbcf2b9b318c488c470bdfba1bc6
Lior-kanomi <lior.kanomi@gmail.com>
2023-05-17 16:50:44 +0300

    Remove google  trends

--- routes/googleTrendsRoute.js
+++ routes/googleTrendsRoute.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const SuggestController = require("../controllers/SuggestController");
 
 // Route for adding a new button
-router.get("/getUserSuggestions/:query", SuggestController.getUserSuggestions);
+router.get("/getGoogleTrends", (req, res) => {});
 
 // Exports the router to be used in the main application
 module.exports = router;
