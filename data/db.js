4c83cfa83bcd4aed8d8fa9933d228354782b9845
Lior-kanomi <lior.kanomi@gmail.com>
2023-05-23 19:19:43 +0300

    Uncomment db block code

--- data/db.js
+++ data/db.js
@@ -18,29 +18,29 @@ mongoose.connection.on("connected", () => {
   console.log("MongoDB connection established!");
 });
 
-mongoose.connection.on("error", (error) => {
-  console.error(`MongoDB connection error: ${error}`);
-  process.exit(-1);
-});
+// mongoose.connection.on("error", (error) => {
+//   console.error(`MongoDB connection error: ${error}`);
+//   process.exit(-1);
+// });
 
-//New Unhandled exceptions method
-mongoose.connection.on("disconnected", (error) => {
-  if (error) {
-    console.error(`MongoDB connection disconnected: ${error}`);
-    process.exit(-1);
-  } else {
-    console.log("MongoDB connection disconnected through app termination");
-    process.exit(0);
-  }
-});
+// //New Unhandled exceptions method
+// mongoose.connection.on("disconnected", (error) => {
+//   if (error) {
+//     console.error(`MongoDB connection disconnected: ${error}`);
+//     process.exit(-1);
+//   } else {
+//     console.log("MongoDB connection disconnected through app termination");
+//     process.exit(0);
+//   }
+// });
 
-// Handle SIGINT event for graceful shutdown
-process.on("SIGINT", (err) => {
-  if (err) {
-    console.error(err);
-  }
-  connection.close(() => {
-    console.log("MongoDB connection closed due to app termination");
-    process.exit(0);
-  });
-});
+// // Handle SIGINT event for graceful shutdown
+// process.on("SIGINT", (err) => {
+//   if (err) {
+//     console.error(err);
+//   }
+//   connection.close(() => {
+//     console.log("MongoDB connection closed due to app termination");
+//     process.exit(0);
+//   });
+// });
