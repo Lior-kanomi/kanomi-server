d70bbc07c49020c435807c6904f439318c5dc40b
Lior Solomon <lior.kanomi@gmail.com>
2023-11-28 14:14:26 +0200

    Remove mixpanel logger

--- routes/mixpanelUserRoute.js
+++ routes/mixpanelUserRoute.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const mixpanelLogger = require("../middlewares/mixpanelLogger"); // Assuming the path is correct
 const mixpanelController = require("../controllers/MixpanelEventController"); // Assuming the path is correct
 
 // Route for adding a new Mixpanel Event
 router.post(
   "/addMixpanelUser",
-  mixpanelLogger,
   mixpanelController.addMixpanelUser
 );
 
