e3c354a32936721a4d00d517a6dfd92aa4311ea2
Lior Solomon <lior.kanomi@gmail.com>
2024-01-02 18:42:35 +0200

    Add routes for staging

--- controllers/ABTestingController.js
+++ controllers/ABTestingController.js
@@ -1,7 +1,7 @@
 const ABTesting = require("../models/ABTesting"); // Assuming the UserAgent
 const statHelper = require("../helpers/selectDocBasedOnStats");
 
-exports.getABTestingGroup = async (req, res) => {
+exports.getABTestingGroups = async (req, res) => {
   try {
     // Query the database to get the current percentages
     const groups = await ABTesting.find({});
@@ -17,6 +17,27 @@ exports.getABTestingGroup = async (req, res) => {
       group: selectedDoc.group,
       desc: selectedDoc.desc,
       behavioralGroup: "Power user"
+    });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
+// For staging environment.
+exports.getABTestingGroup = async (req, res) => {
+  try {
+    // Query the database to get the current percentages
+    const groups = await ABTesting.find({});
+
+    if (!groups || groups.length === 0) {
+      res.status(404).send("No documents found");
+      return;
+    }
+
+    const selectedDoc = statHelper.selectDocBasedOnStats(groups) || "A";
+    res.status(201).json({
+      message: "Success",
+      group: selectedDoc.group,
+      desc: selectedDoc.desc
     });
   } catch (error) {
     res.status(500).json({ error });
@@ -48,6 +69,39 @@ exports.addStatsField = async (req, res) => {
 };
 
 exports.updateGroupField = async (req, res) => {
+  try {
+    const { desc, id, group } = req.query;
+
+    const updatedDoc = await ABTesting.findOne({ group: "A" }); // TODO: Validate the id in Mixpanel so you will know the is the same group in both
+    // TODO: Add the logic for replacing the group from one to another...
+
+    // Update the 'desc' field of the document matching the provided group
+    if (!updatedDoc) {
+      const defaultdDoc = await ABTesting.findOne({ group: "A" });
+      return res.status(200).json({
+        shouldUpdate: true,
+        message: "Default ab testing object returned",
+        group: defaultdDoc.group || "A",
+        desc: updatedDoc.desc || "Default group"
+      });
+    }
+
+    return res.status(200).json({
+      shouldUpdate: true,
+      message: "Updated ab testing object returned",
+      group: updatedDoc.group,
+      desc: updatedDoc.desc
+    });
+  } catch (error) {
+    res.status(500).json({
+      shouldUpdate: false,
+      error: "An error occurred while updating the description field.",
+      details: error.message
+    });
+  }
+};
+// For staging environment.
+exports.updateGroups = async (req, res) => {
   try {
     const { desc, id, group, behavioralGroup } = req.query;
 
