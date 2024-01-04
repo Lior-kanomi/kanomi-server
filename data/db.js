a949a5e70438797e7dd6567bc9a75eb85657f548
Dotan Veretzky <dotan@kanomi.tech>
2024-01-04 15:24:21 +0200

    Updated the comments in db.js for better readability. This update is primarily aimed at verifying the git push for my credentials.
    Signed-off-by: Dotan Veretzky

--- data/db.js
+++ data/db.js
@@ -1,15 +1,21 @@
+// Import required modules
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+// Set mongoose to throw errors instead of warnings
 mongoose.set("strictQuery", true);
 
+// Determine the database URI based on the environment
 const isDev = process.env.NODE_ENV !== "production";
 const dbURI = isDev ? process.env.MONGODB_URI_DEV : process.env.MONGODB_URI;
+
+// Connect to the MongoDB database using the URI
 mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+ useNewUrlParser: true,
+ useUnifiedTopology: true,
 });
 
+// Log a message when the connection is established
 mongoose.connection.on("connected", () => {
-  console.log("MongoDB connection established!");
+ console.log("MongoDB connection established!");
 });
